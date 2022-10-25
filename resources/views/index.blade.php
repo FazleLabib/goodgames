05fe5797753b418fa2bc48b4b3daea405ad3494b
Falze Labib <fazle.labib@gmail.com>
2022-10-25 16:24:28 +0600

    Created Landing Page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GoodGames</title>
+    <!-- <link rel="stylesheet" href = "css/app.css"/> -->
+    <!--<link href="{{ asset('C:\Users\Fazle Labib\goodgames\resources\css\app.css') }}" rel="stylesheet">-->
+    <link href="{{ asset('/css/styles.css') }}" rel="stylesheet">
+    
+</head>
+<body>
+    <section class = "main">
+        <div id = "grad"></div>
+        <nav>
+            <a href = "#" class = "logo">
+                <img src = "images/logo.png"/>
+            </a>
+
+            <ul class = "menu"> 
+                <li><a href = "#">SIGN IN</a></li>
+                <li><a href = "#">REGISTER</a></li>
+            </ul>
+        </nav>
+        <div class = "heading">
+            <h1>Log and rate games you've played.<br />Find your next favorite game.</h1>
+            <button type = "button">GET STARTED</button>
+
+        </div>
+    </section>
+</body>
+</html>
