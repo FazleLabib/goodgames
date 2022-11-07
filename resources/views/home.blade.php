ccb008deb781853cac3e9f00705ba41d2fdbb449
Falze Labib <fazle.labib@gmail.com>
2022-11-07 15:15:10 +0600

    Created Separate blade file for header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Good Games</title>
-    <link href="{{ asset('/css/styles.css') }}" rel="stylesheet">
-</head>
+@include('partials.header')
 
 <body>
     @if (isset(Auth::user()->email))
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Good Games</title>
-    <link href="{{ asset('/css/styles.css') }}" rel="stylesheet">
-</head>
+@include('partials.header')
+
 @if(isset(Auth::user()->email))
         <script>window.location="home";</script>
 @endif
