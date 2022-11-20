f380331b353d9d9593a7b1be6397f68fecb265b4
Falze Labib <fazle.labib@gmail.com>
2022-11-20 17:02:29 +0600

    Created Model for list_contains table

---
+++ app/Models/ListContain.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListContain extends Model
+{
+    use HasFactory;
+}
